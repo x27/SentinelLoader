8d3da8e86ede771d15e433e6f6d2b81e0d7762f6
x27 <xinurbah@gmail.com>
2025-12-07 15:20:27 +1000

    added sources

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace SentinelLoader
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            Dictionary<uint, uint> d = new Dictionary<uint, uint>
+            {
+                { 512000000u, 757999999u },
+                { 823987500u, 849012499u },
+                { 868987500u, 894012499u },
+                { 960000000u, 1239999999u },
+            };
+
+            try
+            {
+                Assembly asm = Assembly.LoadFrom("BCDx36HP_Sentinel.exe");
+                var freqLibType = asm.GetType("HomePatrol_Sentinel.FrequencyLib");
+                var freqTableField = freqLibType.GetField("FrequencyTable", BindingFlags.Static | BindingFlags.NonPublic);
+                var freqTable = freqTableField.GetValue(null) as uint[,];
+                for (var i = 0; i < freqTable.GetLength(0); i++)
+                {
+                    if (d.ContainsKey(freqTable[i, 1]))
+                    {
+                        freqTable[i, 1] = d[freqTable[i, 1]];
+                    }
+                }
+                asm.EntryPoint.Invoke(null, null);
+            }
+            catch
+            {
+                MessageBox.Show("Something wrong!","Exception");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SentinelLoader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SentinelLoader")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd5be169-5e7e-4d5a-9ea4-5c72d8da2413")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
